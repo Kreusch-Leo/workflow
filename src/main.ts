a69040d50bde9f2dbfee63fd4520a4fbae8f7e8a
Leonardo Kreusch <kreuschleoanc@gmail.com>
2025-08-26 00:55:07 -0300

    FIX: page creation

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,32 @@
-const { app, BrowserWindow } = require("electron");
+import { app, BrowserWindow, ipcMain } from "electron";
+import * as path from "path";
+import { PAGE_PATH, WINDOWS, DEFAULT_SIZE } from "./constants";
 
-let mainWindow: any = null;
+let win: BrowserWindow | null = null;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: { nodeIntegration: true },
+function createWindow(file: string) {
+  win = new BrowserWindow({
+    width: DEFAULT_SIZE.WIDTH,
+    height: DEFAULT_SIZE.HEIGHT,
+    webPreferences: {
+      preload: path.resolve(__dirname, "preload.js"),
+      nodeIntegration: true
+    }
   });
 
-  mainWindow.loadFile("src/index.html");
-
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+  win.loadFile(path.join(__dirname, PAGE_PATH, file));
+  return win;
 }
 
-app.on("ready", createWindow);
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.whenReady().then(() => {
+  win = createWindow(WINDOWS.INDEX);
 });
 
-app.on("activate", () => {
-  if (mainWindow === null) createWindow();
+ipcMain.on("open-window", (event, file: string) => {
+  if (win) win.close();
+  win = createWindow(file);
+});
+
+ipcMain.on("quit-app", () => {
+  app.quit();
 });
