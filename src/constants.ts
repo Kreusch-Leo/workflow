cc50bf59e12c237b204fb64732d2f03fee9480b0
Leonardo Kreusch <kreuschleoanc@gmail.com>
2025-08-26 00:52:57 -0300

    ADD: global constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const APP_NAME = "Workflow"
+
+export const PAGE_PATH = "pages"
+
+export const DEFAULT_SIZE = {
+  WIDTH: 1200,
+  HEIGHT: 900
+} as const;
+
+export const WINDOWS = {
+  INDEX: "index.html",
+  NEW_PROJECT: "new_project.html",
+  SELECT_PROJECT: "select_project.html",
+} as const;
